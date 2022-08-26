01b65116ad630b5f1afab469910ab4d85b3782b6
Gopichand <nallanigopichand@gmail.com>
2022-08-25 18:52:40 -0700

    Modified Create ec2-instance.py

--- createec2.py
+++ createec2.py
@@ -2,21 +2,24 @@ import boto3
 
 ec2 = boto3.resource('ec2')
 
+Tags = [
+        {
+            'ResourceType': 'instance',
+            'Tags': [
+                {
+                    'Key': 'Owner',
+                    'Value': 'Boto3-server'
+                },
+            ]
+        },
+    ]
+
 instance = ec2.create_instances(
     ImageId='ami-052efd3df9dad4825',
     InstanceType='t2.micro',
     KeyName='prodkey',
     MinCount=1,
     MaxCount=1,
-    TagSpecifications=[
-        {
-            'ResourceType': 'instance',
-            'Tags': [
-                {
-                    'Key': 'Name',
-                    'Value': 'Boto3-server'
-                },
-            ]
-        },
-    ],
+    TagSpecifications=Tags
 )
+
